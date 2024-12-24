d45865b5346c0cc0577df96e50c90cbb7f3eb7b5
Макс <rumyantsev1m0s1@gmail.com>
2024-12-24 21:47:29 +0300

    Добавлены недостающие экспорты (в __init__.py) важных элементов проекта

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+__all__ = (
+    "async_session_maker",
+    "Base",
+    "lg"
+)
+
+from .core import async_session_maker, Base
+from .logging_cfg import lg
---
+++ app/core/__init__.py
@@ -0,0 +1,6 @@
+__all__ = (
+    "async_session_maker",
+    "Base"
+)
+
+from .config import async_session_maker, Base
