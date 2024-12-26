de6cdb99041fba1da8ef980a7b813c9c1d439d5c
Макс <rumyantsev1m0s1@gmail.com>
2024-12-26 22:56:39 +0300

    Изменены названия некоторых столбцов в таблице о характеристиках самолёта

--- app/core/models/aircraft.py
+++ app/core/models/aircraft.py
@@ -7,6 +7,6 @@ class Aircraft(Base):
     __tablename__ = "aircraft"
 
     aircraft_id = Column(type_=Integer, primary_key=True, autoincrement=True)
-    aircraft_name = Column(type_=String(20), nullable=False, index=True)
-    aircraft_weight = Column(type_=Integer, nullable=False)
-    aircraft_length = Column(type_=Integer, nullable=False)
+    name = Column(type_=String(20), nullable=False, index=True)
+    weight = Column(type_=Integer, nullable=False)
+    length = Column(type_=Integer, nullable=False)
