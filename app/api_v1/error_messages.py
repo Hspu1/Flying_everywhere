e1f40cffba8d722c643a788c7e5a673ee001a0cf
Макс <rumyantsev1m0s1@gmail.com>
2025-01-01 20:09:48 +0300

    Изменены некоторые сообщения об ошибках

--- app/api_v1/error_messages.py
+++ app/api_v1/error_messages.py
@@ -11,14 +11,14 @@ def cannot_create_new_object(correction: str):
 def no_data(correction: str):
     raise HTTPException(
         status_code=404,
-        detail=f"В базе данных пока нет ни одного {correction}"
+        detail=f"В базе данных нет ни одного {correction}"
     )
 
 
 def no_object(correction: str):
     raise HTTPException(
         status_code=404,
-        detail=f"В базе данных пока нет {correction} с таким названием"
+        detail=f"В базе данных нет {correction} с таким названием"
     )
 
 
@@ -26,5 +26,5 @@ def cannot_create_flight():
     raise HTTPException(
         status_code=409,
         detail="В базе данных уже есть полёт с таким названием "
-               "или/и в базе данных пока нет самолёта с таким названием"
+               "или/и в базе данных нет самолёта с таким названием"
     )
